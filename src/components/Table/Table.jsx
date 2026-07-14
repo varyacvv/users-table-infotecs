61ea82258e9718c080fa5d25ba8a8475289c5cea
Barbara <varyapezhik@yandex.ru>
2026-07-14 20:51:48 +0700

    Update table layout and prepare sorting

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,39 +1,48 @@
 import styles from "./Table.module.css";
 
 function Table({ users, onSort, sortField, sortOrder }) {
+  function getArrow(field) {
+    if (sortField !== field) return "";
+
+    if (sortOrder === "asc") return " ↑";
+    if (sortOrder === "desc") return " ↓";
+
+    return "";
+  }
+
   return (
     <div className={styles.wrapper}>
       <table className={styles.table}>
         <thead>
-  <tr>
-    <th onClick={() => onSort("fullName")}>
-      ФИО {sortField === "fullName" && (sortOrder === "asc" ? "↑" : sortOrder === "desc" ? "↓" : "")}
-    </th>
+          <tr>
+            <th onClick={() => onSort("fullName")}>
+              ФИО{getArrow("fullName")}
+            </th>
 
-    <th onClick={() => onSort("age")}>
-      Возраст {sortField === "age" && (sortOrder === "asc" ? "↑" : sortOrder === "desc" ? "↓" : "")}
-    </th>
+            <th onClick={() => onSort("age")}>
+              Возраст{getArrow("age")}
+            </th>
 
-    <th onClick={() => onSort("gender")}>
-      Пол {sortField === "gender" && (sortOrder === "asc" ? "↑" : sortOrder === "desc" ? "↓" : "")}
-    </th>
+            <th onClick={() => onSort("gender")}>
+              Пол{getArrow("gender")}
+            </th>
 
-    <th onClick={() => onSort("phone")}>
-      Телефон {sortField === "phone" && (sortOrder === "asc" ? "↑" : sortOrder === "desc" ? "↓" : "")}
-    </th>
+            <th onClick={() => onSort("phone")}>
+              Телефон{getArrow("phone")}
+            </th>
 
-    <th>Email</th>
-    <th>Страна</th>
-    <th>Город</th>
-  </tr>
-</thead>
+            <th>Email</th>
+            <th>Страна</th>
+            <th>Город</th>
+          </tr>
+        </thead>
 
         <tbody>
           {users.map((user) => (
             <tr key={user.id}>
-              <td>{user.lastName}</td>
-              <td>{user.firstName}</td>
-              <td>{user.maidenName}</td>
+              <td>
+                {user.lastName} {user.firstName} {user.maidenName}
+              </td>
               <td>{user.age}</td>
               <td>{user.gender}</td>
               <td>{user.phone}</td>
