76080efa56f8a439554a100306f3f38f8c737d9b
Barbara <varyapezhik@yandex.ru>
2026-07-15 17:22:31 +0700

    Improve API error handling

--- src/api/users.js
+++ src/api/users.js
@@ -1,11 +1,28 @@
 export async function fetchUsers() {
-  const response = await fetch("https://dummyjson.com/users");
+  try {
+    const response = await fetch("https://dummyjson.com/users");
 
-  if (!response.ok) {
-    throw new Error("Не удалось загрузить пользователей");
+    if (!response.ok) {
+      switch (response.status) {
+        case 404:
+          throw new Error("Пользователи не найдены.");
+
+        case 500:
+          throw new Error("Ошибка сервера. Попробуйте позже.");
+
+        default:
+          throw new Error("Не удалось загрузить пользователей.");
+      }
+    }
+
+    const data = await response.json();
+
+    return data.users;
+  } catch (error) {
+    if (error.name === "TypeError") {
+      throw new Error("Нет подключения к интернету.",  { cause: error });
+    }
+
+    throw error;
   }
-
-  const data = await response.json();
-
-  return data.users;
 }
