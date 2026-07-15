be1f470fc9e149d40fa132f1a1a32ca04f421c15
Barbara <varyapezhik@yandex.ru>
2026-07-15 11:45:27 +0700

    Add pagination

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,22 @@ import { useState } from "react";
 import { useUsers } from "./hooks/useUsers";
 import { sortUsers } from "./utils/sortUsers";
 import Table from "./components/Table/Table";
+import Pagination from "./components/Pagination/Pagination";
 
 function App() {
   const { users, loading, error } = useUsers();
 
   const [sortField, setSortField] = useState("");
   const [sortOrder, setSortOrder] = useState("none");
+  const [page, setPage] = useState(1);
+
+  const usersPerPage = 10;
 
   function handleSort(field) {
     if (field !== sortField) {
       setSortField(field);
       setSortOrder("asc");
+      setPage(1); // после сортировки возвращаемся на первую страницу
       return;
     }
 
@@ -24,9 +29,17 @@ function App() {
       setSortField("");
       setSortOrder("none");
     }
+
+    setPage(1);
   }
 
   const sortedUsers = sortUsers(users, sortField, sortOrder);
+
+  const start = (page - 1) * usersPerPage;
+  const end = start + usersPerPage;
+
+  const currentUsers = sortedUsers.slice(start, end);
+  const totalPages = Math.ceil(sortedUsers.length / usersPerPage);
 
   if (loading) {
     return <h2>Загрузка...</h2>;
@@ -41,10 +54,16 @@ function App() {
       <h1>Пользователи</h1>
 
       <Table
-        users={sortedUsers}
+        users={currentUsers}
         onSort={handleSort}
         sortField={sortField}
         sortOrder={sortOrder}
+      />
+
+      <Pagination
+        page={page}
+        totalPages={totalPages}
+        onPageChange={setPage}
       />
     </main>
   );
---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,27 @@
+import styles from "./Pagination.module.css";
+
+function Pagination({ page, totalPages, onPageChange }) {
+  return (
+    <div className={styles.pagination}>
+      <button
+        onClick={() => onPageChange(page - 1)}
+        disabled={page === 1}
+      >
+        ←
+      </button>
+
+      <span>
+        Страница {page} из {totalPages}
+      </span>
+
+      <button
+        onClick={() => onPageChange(page + 1)}
+        disabled={page === totalPages}
+      >
+        →
+      </button>
+    </div>
+  );
+}
+
+export default Pagination;
